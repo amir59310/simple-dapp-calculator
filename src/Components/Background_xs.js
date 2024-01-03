e2ab1d13fed387c6f6c4b4516750387cb6ea26ad
Amirhossein Ghanaatiaan . J <amir59310@gmail.com>
2024-01-03 23:43:33 +0330

    Finished Mobile Display Calculator Componnet

--- src/Components/Background_xs.js
+++ src/Components/Background_xs.js
@@ -3,8 +3,8 @@ const Background_xs = () => {
     return (
 
         <div>
-            <div class="area" >
-                <ul class="circles">
+            <div className="area" >
+                <ul className="circles">
                     <li></li>
                     <li></li>
                     <li></li>
--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -3,24 +3,21 @@ const Calculator = ({ //Props
     PropHandleClickSetNum1, PropHandleClickSetNum2, PropHnadleClickPlus,
     PropHandleClickDevision, PropHandleClickMinus, PropHandleClickMultiplication,
     PropHandleShowResultDivision, PropHandleShowResultMinus, PropHandleShowResultPluse,
-    PropHandleShowResultMultiplication, PropHandleClickShowNum1, PropHandleClickShowNum2 }) => {
+    PropHandleShowResultMultiplication }) => {
 
     return (
 
-        <div className="bg-[#a1a4d66e]  drop-shadow-[0px_15px_20px_rgba(0,6,0,0.35)] rounded-xl pt-4 px-5 " >
+        <div className="bg-[#a1a4d66e]  drop-shadow-[0px_1px_20px_rgba(0,6,0,0.5)] rounded-xl pt-4 px-5 " >
             <div className="">
-                <label className="text-[#191825] font-mono text-sm ml-4 ">
+                <label className="text-[#191825] font-mono font-bold text-sm ml-4 ">
                     Please Set Number
                 </label>
             </div>
             <div className="flex items-center justify-center pb-2 gap-3" >
 
-                <input type="number" value={PropNum1} onChange={(e) => PropSetNum1(e.target.value)}
-                    className="bg-[#a1a4d6] rounded-[0.4rem] px-1 py- dark:text-slate-700  ring-0 focus:outline-none
-                    w-[9rem] focus:drop-shadow-[0px_1px_3px_rgba(0,50,0,0.35)]"></input>
+                <input type="number" value={PropNum1} onChange={(e) => PropSetNum1(e.target.value)} className="bg-[#a1a4d6] rounded-[0.4rem] px-1 py- dark:text-slate-700  ring-0 focus:outline-none w-[9rem] focus:drop-shadow-[0px_1px_3px_rgba(0,50,0,0.35)]"></input>
 
-                <button onClick={PropHandleClickSetNum1} className="
-                bg-[#a1a4d6] text-xs font-mono font-bold py-[0.28rem] px-2 rounded-[0.4rem] text-[#4d497c] " >SET</button>
+                <button onClick={PropHandleClickSetNum1} className=" bg-[#a1a4d6] text-xs font-mono font-bold py-[0.28rem] px-2 rounded-[0.4rem] text-[#4d497c] " >SET</button>
             </div>
             <div className="flex items-center justify-center pb-2 gap-3">
                 <input size={10} value={PropNum2} onChange={(e) => PropSetNum2(e.target.value)} placeholder="" className="
@@ -32,7 +29,7 @@ const Calculator = ({ //Props
 
             <div className="py-4">
                 <div className="">
-                    <label className="text-[#191825] font-mono text-sm  ">
+                    <label className="text-[#191825] font-mono text-sm font-bold ">
                         Calculation Result
                     </label>
                 </div>
@@ -46,7 +43,7 @@ const Calculator = ({ //Props
 
             <div className="bg-[#a1a4d688] rounded-xl flex flex-col items-center">
                 <div>
-                    <label className="text-[#191825] font-mono text-xs font-bold tracking-tight ">Computational operators</label>
+                    <label className="text-[#191825] font-mono text-xs font-bold tracking-tight ">Computational Operators</label>
                 </div>
 
                 <div className="flex items-center justify-center gap-3 pt-1 pb-3">
@@ -58,33 +55,39 @@ const Calculator = ({ //Props
                     <button onClick={PropHnadleClickPlus} className="
                     bg-[#adafe9] px-[0.8rem] pb-1 rounded-[0.6rem] 
                     drop-shadow-[0px_3px_4px_rgba(0,50,0,0.25)]">+</button>
-                    <button onClick={PropHandleClickMinus} className="bg-[#adafe9] px-[0.95rem] pb-[0.3rem] rounded-[0.6rem]
-                    drop-shadow-[0px_3px_4px_rgba(0,50,0,0.25)]" >-</button>
+                    <button onClick={PropHandleClickMinus} className="bg-[#adafe9] px-[0.8rem] pb-[0.3rem] rounded-[0.6rem]
+                    drop-shadow-[0px_3px_4px_rgba(0,50,0,0.25)]" >−</button>
                 </div>
 
             </div>
 
 
+            <div className="bg-[#a1a4d688] rounded-xl flex flex-col items-center mt-4 mb-5 gap-1">
+                <div>
+                    <label className="text-[#191825] font-mono text-xs font-bold tracking-tight ">Calculation Result Operators</label>
+                </div>
+                <div className="flex items-center justify-center gap-[0.7rem] pb-3 ">
+                    <button onClick={PropHandleShowResultDivision} className="bg-[#a1a4d6] text-xs font-mono font-bold px-[0.5rem] py-[0.3rem] rounded-[0.5rem]">
+                        R<span className="bg-[#7f81ac] ml-1 text-center px-[0.3rem] pb-[0.1rem] font-bold rounded-full">×</span></button>
+                    <button onClick={PropHandleShowResultMinus} className="bg-[#a1a4d6] text-xs font-mono font-bold px-[0.5rem] py-[0.3rem] rounded-[0.5rem]">
+                        R<span className="bg-[#7f81ac] ml-1 text-center px-[0.3rem] pb-[0.1rem] rounded-full">–</span></button>
+                    <button onClick={PropHandleShowResultPluse} className="bg-[#a1a4d6] text-xs font-mono font-bold px-[0.5rem] py-[0.3rem] rounded-[0.5rem]">
+                        R<span className="bg-[#7f81ac] ml-1 text-center px-[0.3rem] pb-[0.1rem] rounded-full">+</span></button>
+                    <button onClick={PropHandleShowResultMultiplication} className="bg-[#a1a4d6] text-xs font-mono font-bold px-[0.5rem] py-[0.3rem] rounded-[0.5rem]">
+                        R<span className="bg-[#7f81ac] ml-1  text-center px-[0.3rem] pb-[0.1rem] font-bold rounded-full">×</span></button>
 
-            <div className="flex items-center justify-center gap-[0.65rem] pb-4 ">
-                <button onClick={PropHandleShowResultDivision} className="bg-[#a1a4d6] text-xs font-mono font-bold px-[0.3rem] py-[0.15rem] rounded-[0.5rem]">
-                    R<span className="bg-[#7f81ac] ml-1 text-center px-[0.2rem] pb-[0.1rem] font-bold rounded-full">×</span></button>
-                <button onClick={PropHandleShowResultMinus} className="bg-[#a1a4d6] text-xs font-mono font-bold px-[0.3rem] py-[0.15rem] rounded-[0.5rem]">
-                    R<span className="bg-[#7f81ac] ml-1 text-center px-[0.2rem] pb-[0.1rem] rounded-full">–</span></button>
-                <button onClick={PropHandleShowResultPluse} className="bg-[#a1a4d6] text-xs font-mono font-bold px-[0.3rem] py-[0.15rem] rounded-[0.5rem]">
-                    R<span className="bg-[#7f81ac] ml-1 text-center px-[0.2rem] pb-[0.1rem] rounded-full">+</span></button>
-                <button onClick={PropHandleShowResultMultiplication} className="bg-[#a1a4d6] text-xs font-mono font-bold px-[0.3rem] py-[0.15rem] rounded-[0.5rem]">
-                    R<span className="bg-[#7f81ac] ml-1  text-center px-[0.2rem] pb-[0.1rem] font-bold rounded-full">×</span></button>
-
+                </div>
             </div>
-            <div className="flex items-center justify-center">
-
-            </div>
 
 
 
 
-        </div>
+
+
+
+
+
+        </div >
     )
 }
 
